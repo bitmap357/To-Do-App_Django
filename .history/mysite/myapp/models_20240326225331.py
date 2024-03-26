fd8b3b96a9796dc2d8348ae1eab9e83e13364335
bitmap357 <edakoyeram@gmail.com>
2024-03-26 22:57:08 +0000

    creating model

--- .history/mysite/myapp/models_20240326225331.py
+++ .history/mysite/myapp/models_20240326225331.py
@@ -2,5 +2,4 @@ from django.db import models
 
 # Create your models here.
 class Task(models.Model):
-    name = models.CharField(max_length=100)
-    priority = models.IntegerField()
+    name = models.CharField(_(""), max_length=50)
--- .history/mysite/myapp/models_20240326225346.py
+++ .history/mysite/myapp/models_20240326225346.py
@@ -3,4 +3,3 @@ from django.db import models
 # Create your models here.
 class Task(models.Model):
     name = models.CharField(max_length=100)
-    priority = models.IntegerField()
