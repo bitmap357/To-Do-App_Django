865c57c089743e246c169594454df303cfcae4c1
bitmap357 <edakoyeram@gmail.com>
2024-03-26 23:03:11 +0000

    creating template

--- .history/mysite/myapp/views_20240326225938.py
+++ .history/mysite/myapp/views_20240326225938.py
@@ -3,4 +3,3 @@ from django.shortcuts import render
 # Create your views here.
 def add(request):
     
-    return render(request, 'myapp/add.html')
--- .history/mysite/myapp/views_20240326225945.py
+++ .history/mysite/myapp/views_20240326225945.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def add(request):
-    
-    return render(request, 'myapp/add.html')
+    return render(re)
